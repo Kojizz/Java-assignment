e787c57d5db87cbd93a44aa68fe8f3be790f4ac9
ahmet <ahmetovrasul117@gmail.com>
2025-03-28 20:14:45 +0500

    commit

---
+++ src/Task_5.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+//import java.util.Scanner;
+public class Task_5 {
+    public static int fibonacci(int n) {
+        if (n == 0) return 0; // Base case F(0) = 0
+        if (n == 1) return 1; // Base case F(1) = 1
+        return fibonacci(n - 1) + fibonacci(n - 2); // Recursive formula: F(n) = F(n-1) + F(n-2)
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        System.out.println(fibonacci(n)); // Call recursive function and print result
+    }
+}
